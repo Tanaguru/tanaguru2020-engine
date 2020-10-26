bfe3323897ca7b43e9fbfa5e0a33f349da0c955b
Loan PEDRAU <lpedrau@oceaneconsulting.com>
2020-10-26 18:03:16 +0100

    fix a lazy initialisation exception

--- tanaguru-engine/tanaguru-api/src/main/java/com/tanaguru/repository/ProjectRepository.java
+++ tanaguru-engine/tanaguru-api/src/main/java/com/tanaguru/repository/ProjectRepository.java
@@ -1,8 +1,11 @@
 package com.tanaguru.repository;
 
+import com.tanaguru.domain.entity.audit.Resource;
+import com.tanaguru.domain.entity.audit.Scenario;
 import com.tanaguru.domain.entity.membership.contract.Contract;
 import com.tanaguru.domain.entity.membership.project.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
@@ -13,5 +16,11 @@ import java.util.Collection;
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
     Collection<Project> findAllByContract(Contract contract);
+    
+    @Query("SELECT a FROM Resource a WHERE a.project = ?1")
+    Collection<Resource> getResourcesFromProject(Project project);
+    
+    @Query("SELECT a FROM Scenario a WHERE a.project = ?1")
+    Collection<Scenario> getScenarioFromProject(Project project);
     
 }
