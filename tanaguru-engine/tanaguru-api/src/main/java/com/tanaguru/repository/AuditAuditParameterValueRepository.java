7484697fa7dcfacdd0b533dcce428a955a7f1e07
lpedrau <lpedrau@oceaneconsulting.com>
2020-11-06 17:09:10 +0100

    only export PAGE audit for now, in progress

--- tanaguru-engine/tanaguru-api/src/main/java/com/tanaguru/repository/AuditAuditParameterValueRepository.java
+++ tanaguru-engine/tanaguru-api/src/main/java/com/tanaguru/repository/AuditAuditParameterValueRepository.java
@@ -1,9 +1,15 @@
 package com.tanaguru.repository;
 
 import com.tanaguru.domain.entity.audit.Audit;
+import com.tanaguru.domain.entity.audit.AuditReference;
 import com.tanaguru.domain.entity.audit.parameter.AuditAuditParameterValue;
+
+import java.util.Collection;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuditAuditParameterValueRepository extends JpaRepository<AuditAuditParameterValue, Long>  {
     void deleteAllByAudit(Audit audit);
+    Collection<AuditAuditParameterValue> findAllByAudit(Audit audit);
 }
--- tanaguru-rest/src/main/java/com/tanaguru/controller/AuditController.java
+++ tanaguru-rest/src/main/java/com/tanaguru/controller/AuditController.java
@@ -2,22 +2,31 @@ package com.tanaguru.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
-import com.google.gson.JsonIOException;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import com.tanaguru.domain.constant.EAuditParameter;
 import com.tanaguru.domain.constant.EAuditType;
 import com.tanaguru.domain.dto.AuditCommandDTO;
 import com.tanaguru.domain.dto.DemoCommandDTO;
 import com.tanaguru.domain.entity.audit.Audit;
+import com.tanaguru.domain.entity.audit.AuditLog;
+import com.tanaguru.domain.entity.audit.AuditReference;
+import com.tanaguru.domain.entity.audit.AuditScheduler;
+import com.tanaguru.domain.entity.audit.TanaguruTest;
 import com.tanaguru.domain.entity.audit.TestHierarchy;
+import com.tanaguru.domain.entity.audit.parameter.AuditAuditParameterValue;
 import com.tanaguru.domain.entity.membership.Act;
 import com.tanaguru.domain.entity.membership.project.Project;
+import com.tanaguru.domain.entity.pageresult.ElementResult;
+import com.tanaguru.domain.entity.pageresult.StatusResult;
+import com.tanaguru.domain.entity.pageresult.TestResult;
 import com.tanaguru.domain.exception.ForbiddenException;
 import com.tanaguru.domain.exception.InvalidEntityException;
 import com.tanaguru.factory.AuditFactory;
 import com.tanaguru.repository.*;
 import com.tanaguru.service.*;
 import io.swagger.annotations.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -25,16 +34,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
-
 import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.lang.reflect.Array;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -52,6 +57,13 @@ public class AuditController {
     private final ProjectRepository projectRepository;
     private final ActRepository actRepository;
     private final TestHierarchyRepository testHierarchyRepository;
+    private final AuditLogRepository auditLogRepository;
+    private final AuditReferenceRepository auditReferenceRepository;
+    private final AuditAuditParameterValueRepository auditAuditParameterValueRepository;
+    private final AuditSchedulerRepository auditSchedulerRepository;
+    private final StatusResultRepository statusResultRepository;
+    private final TestResultRepository testResultRepository;
+    private final ElementResultRepository elementResultRepository;
 
     @Autowired
     public AuditController(
@@ -60,7 +72,14 @@ public class AuditController {
             AuditRunnerService auditRunnerService,
             ProjectRepository projectRepository,
             ActRepository actRepository,
-            TestHierarchyRepository testHierarchyRepository) {
+            TestHierarchyRepository testHierarchyRepository,
+            AuditLogRepository auditLogRepository,
+            AuditReferenceRepository auditReferenceRepository,
+            AuditAuditParameterValueRepository auditAuditParameterValueRepository,
+            AuditSchedulerRepository auditSchedulerRepository,
+            StatusResultRepository statusResultRepository,
+            TestResultRepository testResultRepository,
+            ElementResultRepository elementResultRepository) {
 
         this.auditRepository = auditRepository;
         this.auditService = auditService;
@@ -69,6 +88,13 @@ public class AuditController {
         this.projectRepository = projectRepository;
         this.actRepository = actRepository;
         this.testHierarchyRepository = testHierarchyRepository;
+        this.auditLogRepository = auditLogRepository;
+        this.auditReferenceRepository = auditReferenceRepository;
+        this.auditAuditParameterValueRepository = auditAuditParameterValueRepository;
+        this.auditSchedulerRepository = auditSchedulerRepository;
+        this.statusResultRepository = statusResultRepository;
+        this.testResultRepository = testResultRepository;
+        this.elementResultRepository = elementResultRepository;
     }
 
     /**
@@ -97,7 +123,7 @@ public class AuditController {
                 .orElseThrow(EntityNotFoundException::new);
     }
 
-    
+
     /**
      * @param id The id of the @see Audit
      * @param shareCode the share code of the @see Audit
@@ -116,30 +142,80 @@ public class AuditController {
     public ResponseEntity<InputStreamResource> exportAudit(
             @PathVariable long id,
             @ApiParam(required = false) @PathVariable(required = false) String shareCode) {
-        
-        Audit audit =  auditRepository.findById(id).orElseThrow(EntityNotFoundException::new);
-        
-        ObjectMapper mapper = new ObjectMapper();   
-        byte[] buf = null;
-        try {
-            buf = mapper.writeValueAsBytes(audit);
-        } catch (JsonProcessingException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
+
+        Audit audit = auditRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        JSONObject jsonFinalObject = new JSONObject();
+
+        if(audit.getType().equals(EAuditType.PAGE)) {
+            Collection<AuditLog> auditLogs =  auditLogRepository.findAllByAudit(audit);
+            Optional<Act> act = actRepository.findByAudit(audit);
+            Optional<AuditReference> auditReference = auditReferenceRepository.findByAuditAndIsMainIsTrue(audit);
+            Collection<AuditAuditParameterValue> auditAuditParameterValues = auditAuditParameterValueRepository.findAllByAudit(audit);
+            Optional<AuditScheduler> auditScheduler = auditSchedulerRepository.findByAudit(audit);
+            ObjectMapper mapper = new ObjectMapper();
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            mapper.setDateFormat(df);
+    
+            try {
+                if(!auditLogs.isEmpty()) {
+                    JSONObject jsonAuditLogObject = new JSONObject();
+                    for(AuditLog log : auditLogs) {
+                        jsonAuditLogObject.append("auditLog", new JSONObject(mapper.writeValueAsString(log)));
+                    }
+                    jsonFinalObject.put("auditLog", jsonAuditLogObject.get("auditLog"));
+                }
+                if(!act.isEmpty()) {
+                    JSONObject jsonActObject = new JSONObject(mapper.writeValueAsString(act.get()));
+                    jsonFinalObject.put("act", jsonActObject);
+                }
+                if(!auditAuditParameterValues.isEmpty()) {
+                    JSONObject jsonAuditParameterObject = new JSONObject();
+                    for(AuditAuditParameterValue auditParameterValue : auditAuditParameterValues) {
+                        jsonAuditParameterObject.append("parameters", new JSONObject(mapper.writeValueAsString(auditParameterValue.getAuditParameterValue())));
+                    }
+                    jsonFinalObject.put("auditParameterValues", jsonAuditParameterObject.get("parameters"));
+                }
+                if(!auditScheduler.isEmpty()) {
+                    jsonFinalObject.put("auditScheduler", mapper.writeValueAsString(auditScheduler.get()));
+                }
+                if(!auditReference.isEmpty()) {
+                    JSONObject jsonAuditReferenceObject = new JSONObject();
+                    AuditReference ref = auditReference.get();
+                    jsonAuditReferenceObject.put("auditReferenceName", ref.getTestHierarchy().getName());
+                    jsonAuditReferenceObject.put("auditReferenceCode", ref.getTestHierarchy().getCode());
+                    jsonAuditReferenceObject.put("auditReferenceUrl", ref.getTestHierarchy().getUrls());    
+                    jsonFinalObject.put("auditReference", jsonAuditReferenceObject);
+    
+                    Collection<StatusResult> statusResults = statusResultRepository.findAllByReferenceAndPage_Audit(auditReference.get().getTestHierarchy(), audit);
+                    for(StatusResult statusResult : statusResults) {
+                        JSONObject jsonStatusResultsObject = new JSONObject();
+                        JSONObject jsonPageStatusResultsObject = new JSONObject(mapper.writeValueAsString(statusResult));
+    
+                        Collection<TestResult> testsResults = testResultRepository.findAllByPage(statusResult.getPage());
+                        for(TestResult testResult : testsResults) {
+                            TanaguruTest tanaguruTest = testResult.getTanaguruTest();                       
+                            JSONObject oneTestResult = new JSONObject(mapper.writeValueAsString(testResult));
+                            JSONArray elementResults = oneTestResult.getJSONArray("elementResults");
+                            List<Long> longs = elementResults.toList().stream()
+                                    .map(object -> Long.parseLong(String.valueOf(object)))
+                                    .collect(Collectors.toList());
+                            Collection<ElementResult> elements = elementResultRepository.findAllByIdIn(longs);
+                            oneTestResult.put("elementResults", new JSONArray(mapper.writeValueAsString(elements)));
+    
+                            oneTestResult.put("tanaguruTest", new JSONObject(mapper.writeValueAsString(tanaguruTest)));
+                            jsonPageStatusResultsObject.append("testResult", oneTestResult);
+                        }
+                        jsonStatusResultsObject.put("page_"+statusResult.getPage().getId(), jsonPageStatusResultsObject);
+                        jsonFinalObject.append("results", jsonStatusResultsObject);
+                    }
+                }
+    
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
         }
-        try {
-            buf = mapper.writeValueAsBytes(audit);
-        } catch (JsonProcessingException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        
-        HttpHeaders header = new HttpHeaders();
-        header.add("Content-Disposition", "attachment; filename=\"audit.json\"");
-        header.add("Cache-Control", "no-cache, no-store, must-revalidate");
-        header.add("Pragma", "no-cache");
-        header.add("Expires", "0");
-        
+        byte[] buf = jsonFinalObject.toString().getBytes();              
+        HttpHeaders header = setUpHeaders();   
         return ResponseEntity
                 .ok()
                 .headers(header)
@@ -147,9 +223,9 @@ public class AuditController {
                 .contentType(
                         MediaType.parseMediaType("application/json"))
                 .body(new InputStreamResource(new ByteArrayInputStream(buf)));
-        
+
     }
-    
+
     /**
      * Get all @see Audit for a given project id
      *
@@ -159,7 +235,7 @@ public class AuditController {
     @ApiOperation(
             value = "Get all audits for a given Project id",
             notes = "User must have SHOW_AUDIT authority on project"
-    )
+            )
     @ApiResponses(value = {
             @ApiResponse(code = 400, message = "Invalid parameters"),
             @ApiResponse(code = 401, message = "Unauthorized"),
@@ -169,7 +245,7 @@ public class AuditController {
     @PreAuthorize(
             "@tanaguruUserDetailsServiceImpl.currentUserHasAuthorityOnProject(" +
                     "T(com.tanaguru.domain.constant.ProjectAuthorityName).SHOW_AUDIT, " +
-                    "#id)")
+            "#id)")
     @GetMapping("/by-project/{id}")
     public @ResponseBody
     Collection<Audit> getAuditsByProject(@PathVariable long id) {
@@ -190,7 +266,7 @@ public class AuditController {
     @PreAuthorize(
             "@tanaguruUserDetailsServiceImpl.currentUserHasAuthorityOnProject(" +
                     "T(com.tanaguru.domain.constant.ProjectAuthorityName).SHOW_AUDIT, " +
-                    "#id)")
+            "#id)")
     @GetMapping("/last-by-project/{id}")
     public @ResponseBody
     Audit getLastAuditByProject(@PathVariable long id) {
@@ -213,7 +289,7 @@ public class AuditController {
     @PreAuthorize(
             "@tanaguruUserDetailsServiceImpl.currentUserHasAuthorityOnProject(" +
                     "T(com.tanaguru.domain.constant.ProjectAuthorityName).SHOW_AUDIT, " +
-                    "#id)")
+            "#id)")
     @GetMapping("/last-by-project/{id}/{type}")
     public @ResponseBody
     Audit getLastAuditByProjectAndAuditType(@PathVariable long id, @PathVariable EAuditType type) {
@@ -269,7 +345,7 @@ public class AuditController {
     @PreAuthorize(
             "@tanaguruUserDetailsServiceImpl.currentUserHasAuthorityOnProject(" +
                     "T(com.tanaguru.domain.constant.ProjectAuthorityName).START_AUDIT, " +
-                    "#auditCommand.getProjectId())")
+            "#auditCommand.getProjectId())")
     @PostMapping("/start")
     public @ResponseBody
     Audit startAudit(@RequestBody @Valid AuditCommandDTO auditCommand) {
@@ -296,14 +372,14 @@ public class AuditController {
         }
 
         Audit audit = auditFactory.createAudit(
-            auditCommand.getName(),
-            auditCommand.getParameters(),
-            auditCommand.getType(),
-            true,
-            project,
-            new ArrayList<>(references),
-            main
-        );
+                auditCommand.getName(),
+                auditCommand.getParameters(),
+                auditCommand.getType(),
+                true,
+                project,
+                new ArrayList<>(references),
+                main
+                );
 
         auditRunnerService.runAudit(audit);
         return audit;
@@ -315,7 +391,7 @@ public class AuditController {
     @ApiOperation(
             value = "Delete an audit by id",
             notes = "User must have DELETE_AUDIT authority on project"
-    )
+            )
     @ApiResponses(value = {
             @ApiResponse(code = 400, message = "Invalid parameters"),
             @ApiResponse(code = 401, message = "Unauthorized"),
@@ -329,5 +405,14 @@ public class AuditController {
     void deleteAudit(@PathVariable long id) {
         auditService.deleteAudit(auditRepository.findById(id)
                 .orElseThrow(EntityNotFoundException::new));
+    }
+
+    private HttpHeaders setUpHeaders() {
+        HttpHeaders header = new HttpHeaders();
+        header.add("Content-Disposition", "attachment; filename=\"audit.json\"");
+        header.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        header.add("Pragma", "no-cache");
+        header.add("Expires", "0");
+        return header;
     }
 }
