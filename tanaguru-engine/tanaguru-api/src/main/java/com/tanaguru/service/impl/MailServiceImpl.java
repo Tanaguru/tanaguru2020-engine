0c765eaa50644e6bb9bf02e2ecba90aded911d8e
rcharre <rcharre@oceaneconsulting.com>
2021-02-11 11:55:16 +0100

    try fix send email by changing mail.from key

--- tanaguru-engine/tanaguru-api/src/main/java/com/tanaguru/service/impl/MailServiceImpl.java
+++ tanaguru-engine/tanaguru-api/src/main/java/com/tanaguru/service/impl/MailServiceImpl.java
@@ -23,9 +23,6 @@ import org.springframework.stereotype.Service;
 public class MailServiceImpl implements MailService {
     private final Logger LOGGER = LoggerFactory.getLogger(MailServiceImpl.class);
 
-    @Value("${mail.from}")
-    private String from;
-
     private final JavaMailSender javaMailSender;
 
     @Autowired
@@ -36,8 +33,6 @@ public class MailServiceImpl implements MailService {
     public void sendSimpleMessage(String to, String subject, String text) throws MailException{
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
-        message.setReplyTo(from);
-        message.setFrom(from);
         message.setSubject(subject);
         message.setText(text);
         javaMailSender.send(message);
@@ -51,8 +46,6 @@ public class MailServiceImpl implements MailService {
         try {
             InternetAddress email = new InternetAddress(to, false);
             helper.setTo(email);
-            helper.setReplyTo(from);
-            helper.setFrom(from);
             helper.setSubject(subject);
             String body = "<!DOCTYPE html><html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/></head><body><p>"+text+"</p></body></html>";
             helper.setText(body, true); //true indicate html support with mime message
