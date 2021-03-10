f97dafd42f710819f0dbb645dcd57c2f56afb1ad
lpedrau <lpedrau@oceaneconsulting.com>
2021-03-10 12:09:01 +0100

    modifications assembly and configuration files

--- tanaguru-entrypoint-cli-docker.sh
+++ tanaguru-entrypoint-cli-docker.sh
@@ -8,7 +8,7 @@ echo "Container - Init Script - Tanaguru initialization"
 echo
 
 echo "Apply tanaguru cli configuration"
-sed -i "s;spring.main.web-application-type.*;spring.main.web-application-type=${WEB_ENV};g" /opt/tanaguru-cli-docker/config/common.properties
+sed -i "s;spring.main.web-application-type.*;spring.main.web-application-type=${WEB_ENV};g" /opt/tanaguru-cli-docker/config/audit-docker.properties
 sed -i "s;spring.datasource.url.*;spring.datasource.url=${DB_URL};g" /opt/tanaguru-cli-docker/config/common.properties
 sed -i "s;spring.jpa.properties.hibernate.default_schema.*;spring.jpa.properties.hibernate.default_schema=${DB_SCHEMA};g" /opt/tanaguru-cli-docker/config/common.properties
 sed -i "s;spring.datasource.username.*;spring.datasource.username=${DB_USERNAME};g" /opt/tanaguru-cli-docker/config/common.properties
@@ -23,24 +23,24 @@ sed -i "s;spring.mail.properties.mail.smtp.auth.*;spring.mail.properties.mail.sm
 sed -i "s;crypto.key.*;crypto.key=${CRYPTO_KEY};g" /opt/tanaguru-cli-docker/config/common.properties
 sed -i "s;password.tokenValidity.*;password.tokenValidity=${PASSWORD_TOKEN_VALIDITY};g" /opt/tanaguru-cli-docker/config/common.properties
 
-sed -i "s;auditrunner.chrome-binary.*;auditrunner.chrome-binary=/usr/bin/google-chrome;g" /opt/tanaguru-cli-docker/config/audit-runner.properties
-sed -i "s;auditrunner.chromedriver.*;auditrunner.chromedriver=/usr/bin/chromedriver;g" /opt/tanaguru-cli-docker/config/audit-runner.properties
-sed -i "s;auditrunner.firefox-binary.*;auditrunner.firefox-binary=/opt/firefox/firefox;g" /opt/tanaguru-cli-docker/config/audit-runner.properties
-sed -i "s;auditrunner.geckodriver.*;auditrunner.geckodriver=/opt/geckodriver;g" /opt/tanaguru-cli-docker/config/audit-runner.properties
-sed -i "s;auditrunner.active.*;auditrunner.active=firefox,chrome;g" /opt/tanaguru-cli-docker/config/audit-runner.properties
+sed -i "s;auditrunner.chrome-binary.*;auditrunner.chrome-binary=/usr/bin/google-chrome;g" /opt/tanaguru-cli-docker/config/audit-docker.properties
+sed -i "s;auditrunner.chromedriver.*;auditrunner.chromedriver=/usr/bin/chromedriver;g" /opt/tanaguru-cli-docker/config/audit-docker.properties
+sed -i "s;auditrunner.firefox-binary.*;auditrunner.firefox-binary=/opt/firefox/firefox;g" /opt/tanaguru-cli-docker/config/audit-docker.properties
+sed -i "s;auditrunner.geckodriver.*;auditrunner.geckodriver=/opt/geckodriver;g" /opt/tanaguru-cli-docker/config/audit-docker.properties
+sed -i "s;auditrunner.active.*;auditrunner.active=firefox,chrome;g" /opt/tanaguru-cli-docker/config/audit-docker.properties
 
-sed -i "s;auditrunner.proxy.exclusionUrls.*;auditrunner.proxy.exclusionUrls=${AUDITRUNNER_PROXY_EXCLUSION_URLS};g" /opt/tanaguru-cli-docker/config/audit-runner.properties
-sed -i "s;auditrunner.proxy.username.*;auditrunner.proxy.username=${AUDITRUNNER_PROXY_USERNAME};g" /opt/tanaguru-cli-docker/config/audit-runner.properties
-sed -i "s;auditrunner.proxy.password.*;auditrunner.proxy.password=${AUDITRUNNER_PROXY_PASSWORD};g" /opt/tanaguru-cli-docker/config/audit-runner.properties
-sed -i "s;auditrunner.proxy.port.*;auditrunner.proxy.port=${AUDITRUNNER_PROXY_PORT};g" /opt/tanaguru-cli-docker/config/audit-runner.properties
-sed -i "s;auditrunner.proxy.host.*;auditrunner.proxy.host=${AUDITRUNNER_PROXY_HOST};g" /opt/tanaguru-cli-docker/config/audit-runner.properties
-sed -i "s;auditrunner.implicitlyWait.*;auditrunner.implicitlyWait=${AUDITRUNNER_IMPLICIT_WAIT};g" /opt/tanaguru-cli-docker/config/audit-runner.properties
-sed -i "s;auditrunner.pageLoadTimeout.*;auditrunner.pageLoadTimeout=${AUDITRUNNER_PAGE_LOAD_TIMEOUT};g" /opt/tanaguru-cli-docker/config/audit-runner.properties
-sed -i "s;auditrunner.scriptTimeout.*;auditrunner.scriptTimeout=${AUDITRUNNER_SCRIPT_TIMEOUT};g" /opt/tanaguru-cli-docker/config/audit-runner.properties
-sed -i "s;auditrunner.firefox.profile.*;auditrunner.firefox.profile=${AUDITRUNNER_FIREFOX_PROFILE};g" /opt/tanaguru-cli-docker/config/audit-runner.properties
-sed -i "s;auditrunner.chrome.profile.*;auditrunner.chrome.profile=${AUDITRUNNER_CHROME_PROFILE};g" /opt/tanaguru-cli-docker/config/audit-runner.properties
-sed -i "s;auditrunner.maxConcurrentAudit.*;auditrunner.maxConcurrentAudit=${AUDITRUNNER_MAX_CONCURRENT_AUDITS};g" /opt/tanaguru-cli-docker/config/audit-runner.properties
-sed -i "s;auditrunner.profile.*;auditrunner.profile=${AUDITRUNNER_PROFILE};g" /opt/tanaguru-cli-docker/config/audit-runner.properties
+sed -i "s;auditrunner.proxy.exclusionUrls.*;auditrunner.proxy.exclusionUrls=${AUDITRUNNER_PROXY_EXCLUSION_URLS};g" /opt/tanaguru-cli-docker/config/audit-docker.properties
+sed -i "s;auditrunner.proxy.username.*;auditrunner.proxy.username=${AUDITRUNNER_PROXY_USERNAME};g" /opt/tanaguru-cli-docker/config/audit-docker.properties
+sed -i "s;auditrunner.proxy.password.*;auditrunner.proxy.password=${AUDITRUNNER_PROXY_PASSWORD};g" /opt/tanaguru-cli-docker/config/audit-docker.properties
+sed -i "s;auditrunner.proxy.port.*;auditrunner.proxy.port=${AUDITRUNNER_PROXY_PORT};g" /opt/tanaguru-cli-docker/config/audit-docker.properties
+sed -i "s;auditrunner.proxy.host.*;auditrunner.proxy.host=${AUDITRUNNER_PROXY_HOST};g" /opt/tanaguru-cli-docker/config/audit-docker.properties
+sed -i "s;auditrunner.implicitlyWait.*;auditrunner.implicitlyWait=${AUDITRUNNER_IMPLICIT_WAIT};g" /opt/tanaguru-cli-docker/config/audit-docker.properties
+sed -i "s;auditrunner.pageLoadTimeout.*;auditrunner.pageLoadTimeout=${AUDITRUNNER_PAGE_LOAD_TIMEOUT};g" /opt/tanaguru-cli-docker/config/audit-docker.properties
+sed -i "s;auditrunner.scriptTimeout.*;auditrunner.scriptTimeout=${AUDITRUNNER_SCRIPT_TIMEOUT};g" /opt/tanaguru-cli-docker/config/audit-docker.properties
+sed -i "s;auditrunner.firefox.profile.*;auditrunner.firefox.profile=${AUDITRUNNER_FIREFOX_PROFILE};g" /opt/tanaguru-cli-docker/config/audit-docker.properties
+sed -i "s;auditrunner.chrome.profile.*;auditrunner.chrome.profile=${AUDITRUNNER_CHROME_PROFILE};g" /opt/tanaguru-cli-docker/config/audit-docker.properties
+sed -i "s;auditrunner.maxConcurrentAudit.*;auditrunner.maxConcurrentAudit=${AUDITRUNNER_MAX_CONCURRENT_AUDITS};g" /opt/tanaguru-cli-docker/config/audit-docker.properties
+sed -i "s;auditrunner.profile.*;auditrunner.profile=${AUDITRUNNER_PROFILE};g" /opt/tanaguru-cli-docker/config/audit-docker.properties
 
 echo "Launch Tanaguru CLI Custom"
 chmod +x /opt/tanaguru-cli-docker/src/main/resources/bin/tanaguru-cli-docker.sh
