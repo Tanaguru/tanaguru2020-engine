acc797a204cae158ced11e2122b2b16c2a60f02d
lpedrau <lpedrau@oceaneconsulting.com>
2021-03-11 11:45:25 +0100

    endpoint filter logs by date and/or level and endpoint logs levels

--- tanaguru-engine/tanaguru-api/src/main/java/com/tanaguru/repository/AuditLogRepository.java
+++ tanaguru-engine/tanaguru-api/src/main/java/com/tanaguru/repository/AuditLogRepository.java
@@ -4,13 +4,12 @@ import com.tanaguru.domain.constant.EAuditLogLevel;
 import com.tanaguru.domain.entity.audit.Audit;
 import com.tanaguru.domain.entity.audit.AuditLog;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
-import java.util.Date;
 
 /**
  * @author rcharre
@@ -20,7 +19,11 @@ public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
     Collection<AuditLog> findAllByAudit(Audit audit);
     Page<AuditLog> findAllByAudit(Audit audit, Pageable pageable);
     Page<AuditLog> findAllByAuditAndLevel(Audit audit, EAuditLogLevel level, Pageable pageable);
-    Page<AuditLog> findAllByAuditAndDate(Audit audit, Date date, Pageable pageable);
-    Page<AuditLog> findAllByAuditAndLevelAndDate(Audit audit, EAuditLogLevel level,Date date ,Pageable pageable);
+    
+    @Query("select e from AuditLog e where e.audit = ?1 and year(e.date) = ?2 and month(e.date) = ?3 and day(e.date)= ?4 ")
+    Page<AuditLog> findAllByAuditAndDate(Audit audit, int year, int month, int day, Pageable pageable);
+    
+    @Query("select e from AuditLog e where e.audit = ?1 and e.level = ?2 and year(e.date) = ?3 and month(e.date) = ?4 and day(e.date)= ?5 ")
+    Page<AuditLog> findAllByAuditAndLevelAndDate(Audit audit, EAuditLogLevel level, int year, int month, int day , Pageable pageable);
 
 }
--- tanaguru-rest/src/main/java/com/tanaguru/controller/AuditLogController.java
+++ tanaguru-rest/src/main/java/com/tanaguru/controller/AuditLogController.java
@@ -6,6 +6,7 @@ import com.tanaguru.domain.exception.CustomEntityNotFoundException;
 import com.tanaguru.domain.exception.CustomInvalidArgumentException;
 import com.tanaguru.domain.entity.audit.Audit;
 import com.tanaguru.domain.entity.audit.AuditLog;
+import com.tanaguru.domain.entity.audit.parameter.AuditParameterValue;
 import com.tanaguru.repository.AuditLogRepository;
 import com.tanaguru.repository.AuditRepository;
 import io.swagger.annotations.ApiOperation;
@@ -14,6 +15,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.Date;
 import java.util.Optional;
 
@@ -75,9 +77,10 @@ public class AuditLogController {
      * @return A list of @see AuditLog
      */
     @ApiOperation(
-            value = "Get paginable AuditLog for a given Audit id",
+            value = "Get paginable AuditLog for a given Audit id filtered by date and/or level",
             notes = "User must have SHOW_AUDIT authority on audit's project or a valid sharecode"
-                    + "\nIf audit not found, exception raise : AUDIT_NOT_FOUND with audit id"
+                    + "\nIf audit not found, exception raise : AUDIT_NOT_FOUND with audit id "
+                    + "\nDate format must be : dd-MM-yyyy"
     )
     @ApiResponses(value = {
             @ApiResponse(code = 400, message = "Invalid parameters"),
@@ -85,8 +88,8 @@ public class AuditLogController {
             @ApiResponse(code = 403, message = "Forbidden for current session or invalid sharecode"),
             @ApiResponse(code = 404, message = "Audit not found : AUDIT_NOT_FOUND error")
     })
-    /**@PreAuthorize(
-            "@tanaguruUserDetailsServiceImpl.currentUserCanShowAudit(#id, #shareCode)")**/
+    @PreAuthorize(
+            "@tanaguruUserDetailsServiceImpl.currentUserCanShowAudit(#id, #shareCode)")
     @GetMapping("/by-audit-filtered/{id}/{shareCode}")
     public @ResponseBody
     Page<AuditLog> getAuditLogByAuditFiltered(
@@ -94,29 +97,41 @@ public class AuditLogController {
             @PathVariable(required = false) @ApiParam(required = false) String shareCode,
             @RequestParam(defaultValue = "0") @ApiParam(required = false) int page,
             @RequestParam(defaultValue = "10") @ApiParam(required = false) int size,
-            @RequestParam(value="date",required = false) @DateTimeFormat(pattern="dd-MM-yyyy") Optional<LocalDate> date,
-            @RequestParam(defaultValue = "INFO") @ApiParam(required = false) Optional<String> level) {
+            @RequestParam(required = false) @DateTimeFormat(pattern="dd-MM-yyyy") Optional<LocalDate> date,
+            @RequestParam(required = false) @ApiParam(required = false) Optional<EAuditLogLevel> level) {
+        
         Audit audit = auditRepository.findById(id)
                 .orElseThrow(() -> new CustomEntityNotFoundException(CustomError.AUDIT_NOT_FOUND, id ));
         
-        EAuditLogLevel lvl = null;
         Page<AuditLog> logs = null;
-        try {
-            lvl = EAuditLogLevel.valueOf(level.get());
-        }catch(IllegalArgumentException e) {
-            throw new CustomInvalidArgumentException(CustomError.LOG_LEVEL_NOT_FOUND, level.get());
-        } 
         
         if(level.isPresent() && date.isPresent()) {
-            logs = auditLogRepository.findAllByAuditAndLevelAndDate(audit,lvl, java.sql.Date.valueOf(date.get()),PageRequest.of(page, size, Sort.by("date"))); 
+            LocalDate localDate = date.get();
+            logs = auditLogRepository.findAllByAuditAndLevelAndDate(audit,level.get(), localDate.getYear(), localDate.getMonthValue(), localDate.getDayOfMonth(),PageRequest.of(page, size, Sort.by("date"))); 
         }else if(level.isPresent()) {
-            logs = auditLogRepository.findAllByAuditAndLevel(audit,lvl,PageRequest.of(page, size, Sort.by("date"))); 
+            logs = auditLogRepository.findAllByAuditAndLevel(audit,level.get(),PageRequest.of(page, size, Sort.by("date"))); 
         }else if(date.isPresent()) {
-            logs = auditLogRepository.findAllByAuditAndDate(audit,java.sql.Date.valueOf(date.get()),PageRequest.of(page, size, Sort.by("date"))); 
+            LocalDate localDate = date.get();
+            logs = auditLogRepository.findAllByAuditAndDate(audit, localDate.getYear(), localDate.getMonthValue(), localDate.getDayOfMonth(),PageRequest.of(page, size, Sort.by("date"))); 
         }else {
             logs = auditLogRepository.findAllByAudit(audit,PageRequest.of(page, size, Sort.by("date")));
         }
 
         return logs;
+    }
+    
+    @ApiOperation(
+            value = "Get the different log levels"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Invalid parameters"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden for current session"),
+            @ApiResponse(code = 404, message = "Not found")
+    })
+    @GetMapping("/levels")
+    public @ResponseBody
+    EAuditLogLevel[] getLevels() {
+        return EAuditLogLevel.values();
     }
 }
