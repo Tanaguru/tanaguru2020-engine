79cc16897b31fc5cb2eb99ebd62f42f6e9920d96
rcharre <rcharre@oceaneconsulting.com>
2021-03-23 15:06:10 +0100

    fix project deletion

--- tanaguru-engine/tanaguru-api/src/main/java/com/tanaguru/repository/ActRepository.java
+++ tanaguru-engine/tanaguru-api/src/main/java/com/tanaguru/repository/ActRepository.java
@@ -17,6 +17,13 @@ public interface ActRepository extends JpaRepository<Act, Long> {
      * @param project The project to delete acts from
      */
     void deleteAllByProject(Project project);
+
+    /**
+     * Delete an act for a given Audit
+     * @param audit The given audit
+     */
+    void deleteByAudit(Audit audit);
+
     Optional<Act> findByAudit(Audit audit);
 
     Collection<Act> findAllByProject(Project project);
