432abd40881461352304992f2223d49b6fd33d80
lpedrau <lpedrau@oceaneconsulting.com>
2021-06-09 15:37:10 +0200

    add DTO classes for the sso module

---
+++ tanaguru-engine/tanaguru-api/src/main/java/com/tanaguru/domain/dto/AccessTokenDTO.java
@@ -0,0 +1,55 @@
+package com.tanaguru.domain.dto;
+
+public class AccessTokenDTO {
+
+	private String access_token;
+	
+	private String refresh_token;
+	
+	private String scope;
+	
+	private String created_at;
+	
+	private String token_type;
+
+	public String getAccess_token() {
+		return access_token;
+	}
+
+	public void setAccess_token(String access_token) {
+		this.access_token = access_token;
+	}
+
+	public String getRefresh_token() {
+		return refresh_token;
+	}
+
+	public void setRefresh_token(String refresh_token) {
+		this.refresh_token = refresh_token;
+	}
+
+	public String getScope() {
+		return scope;
+	}
+
+	public void setScope(String scope) {
+		this.scope = scope;
+	}
+
+	public String getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(String created_at) {
+		this.created_at = created_at;
+	}
+
+	public String getToken_type() {
+		return token_type;
+	}
+
+	public void setToken_type(String token_type) {
+		this.token_type = token_type;
+	}
+	
+}
