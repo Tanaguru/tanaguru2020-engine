728449ec4c12d080b3cfb56fc728a22263174b24
Raphaël Charre <rcharre@oceaneconsulting.com>
2021-07-23 11:27:35 +0200

    fix content.js

--- tanaguru-engine/tanaguru-audit-runner/src/main/resources/scripts/content.js
+++ tanaguru-engine/tanaguru-audit-runner/src/main/resources/scripts/content.js
@@ -1684,9 +1684,7 @@ function loadTanaguruTests() {
     for (var tag in window.tanaguru.tags) {
         tags.push(window.tanaguru.tags[tag]);
     }
-    tags = tags.sort(function (a, b) {
-        return a.name.localeCompare(b.name);
-    });
+
     var result = { tags: tags, tests: window.tanaguru.tests };
     window.tanaguru = undefined;
     return result;
@@ -1713,7 +1711,7 @@ function manageOutput(element) {
 
 function createTanaguruTag(tag, status) {
     if (!window.tanaguru.tags[tag]) {
-        window.tanaguru.tags[tag] = { id: tag, name: 'tag' + tag.charAt(0).toUpperCase() + tag.slice(1), status: status, nbfailures: 0 };
+        window.tanaguru.tags[tag] = { id: tag, status: status, nbfailures: 0 };
     }
 }
 
