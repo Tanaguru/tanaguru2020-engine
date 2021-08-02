e384c58f8b718cfea5c8985e1b2dc356d6b18656
lpedrau <lpedrau@oceaneconsulting.com>
2021-08-02 10:46:08 +0200

    add logger error controllerAdvice

--- tanaguru-rest/src/main/java/com/tanaguru/config/TanaguruControllerAdvice.java
+++ tanaguru-rest/src/main/java/com/tanaguru/config/TanaguruControllerAdvice.java
@@ -7,6 +7,9 @@ import com.tanaguru.domain.exception.CustomInvalidEntityException;
 import com.tanaguru.domain.exception.CustomEntityNotFoundException;
 import com.tanaguru.domain.exception.CustomForbiddenException;
 import com.tanaguru.domain.exception.CustomInvalidEntityException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.LockedException;
@@ -23,11 +26,14 @@ import java.util.NoSuchElementException;
  */
 @ControllerAdvice
 public class TanaguruControllerAdvice {
+    
+    private final Logger LOGGER = LoggerFactory.getLogger(TanaguruControllerAdvice.class);
 
     @ResponseBody
     @ExceptionHandler(CustomEntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     ErrorDTO customEntityNotFoundHandler(CustomEntityNotFoundException ex) {
+        LOGGER.error(ex.getMessage());
         return new ErrorDTO(ex.getMessage(),ex.getContent());
     }
 
@@ -35,6 +41,7 @@ public class TanaguruControllerAdvice {
     @ExceptionHandler(NoSuchElementException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     ErrorDTO noSuchElementExceptionHandler(NoSuchElementException ex) {
+        LOGGER.error(ex.getMessage());
         return new ErrorDTO(ex.getMessage());
     }
 
@@ -42,6 +49,7 @@ public class TanaguruControllerAdvice {
     @ExceptionHandler(CustomForbiddenException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
     ErrorDTO entityForbidden(CustomForbiddenException ex) {
+        LOGGER.error(ex.getMessage());
         return new ErrorDTO(ex.getMessage(), ex.getContent());
     }
 
@@ -49,6 +57,7 @@ public class TanaguruControllerAdvice {
     @ExceptionHandler(BadCredentialsException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     ErrorDTO badCredential(BadCredentialsException ex) {
+        LOGGER.error(ex.getMessage());
         return new ErrorDTO(ex.getMessage());
     }
 
@@ -56,6 +65,7 @@ public class TanaguruControllerAdvice {
     @ExceptionHandler(HttpClientErrorException.Unauthorized.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     ErrorDTO entityForbidden(HttpClientErrorException.Unauthorized ex) {
+        LOGGER.error(ex.getMessage());
         return new ErrorDTO(ex.getMessage());
     }
 
@@ -63,6 +73,7 @@ public class TanaguruControllerAdvice {
     @ExceptionHandler(InternalError.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     ErrorDTO internalError(InternalError ex) {
+        LOGGER.error(ex.getMessage());
         return new ErrorDTO(ex.getMessage());
     }
 
@@ -70,6 +81,7 @@ public class TanaguruControllerAdvice {
     @ExceptionHandler(CustomInvalidEntityException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     ErrorDTO customInvalidEntityError(CustomInvalidEntityException ex) {
+        LOGGER.error(ex.getMessage());
         return new ErrorDTO(ex.getMessage(), ex.getContent());
     }
 
@@ -77,6 +89,7 @@ public class TanaguruControllerAdvice {
     @ExceptionHandler(CustomIllegalStateException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     ErrorDTO customInvalidEntityError(CustomIllegalStateException ex) {
+        LOGGER.error(ex.getMessage());
         return new ErrorDTO(ex.getMessage(), ex.getContent());
     }
     
@@ -84,6 +97,7 @@ public class TanaguruControllerAdvice {
     @ExceptionHandler(LockedException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     ErrorDTO userBlocked(LockedException ex) {
+        LOGGER.error(ex.getMessage());
         return new ErrorDTO(ex.getMessage());
     }
     
@@ -91,6 +105,7 @@ public class TanaguruControllerAdvice {
     @ExceptionHandler(CustomInvalidArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     ErrorDTO customInvalidArgumentError(CustomInvalidArgumentException ex) {
+        LOGGER.error(ex.getMessage());
         return new ErrorDTO(ex.getMessage(), ex.getContent());
     }
 }
