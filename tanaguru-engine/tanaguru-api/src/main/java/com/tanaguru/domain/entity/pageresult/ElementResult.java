52815d9e57d1ddad21c424b78e1777125c28463f
Raphaël Charre <rcharre@oceaneconsulting.com>
2021-08-03 12:12:49 +0200

    update result element

--- tanaguru-engine/tanaguru-api/src/main/java/com/tanaguru/domain/entity/pageresult/ElementResult.java
+++ tanaguru-engine/tanaguru-api/src/main/java/com/tanaguru/domain/entity/pageresult/ElementResult.java
@@ -58,6 +58,18 @@ public class ElementResult implements Serializable {
     @Column
     private String cssSelector;
 
+    @Column
+    private String tag;
+
+    @Column
+    private int size = -1;
+
+    @Column
+    private int weight = -1;
+
+    @Column
+    private float ratio = -1;
+
     public String getAccessibleName() {
         return accessibleName;
     }
@@ -136,5 +148,45 @@ public class ElementResult implements Serializable {
 
     public void setCssSelector(String cssSelector) {
         this.cssSelector = cssSelector;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getRatio() {
+        return ratio;
+    }
+
+    public void setRatio(String ratio) {
+        this.ratio = ratio;
     }
 }
