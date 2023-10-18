cfcc40d5d2b3cd9b103e9f68784b528f666f87e7
severineFF-tanaguru <sfoucherferet@tanaguru.com>
2023-10-18 19:05:47 +0200

    add message.lang property

--- tanaguru-engine/tanaguru-api/src/main/java/com/tanaguru/service/impl/MessageService.java
+++ tanaguru-engine/tanaguru-api/src/main/java/com/tanaguru/service/impl/MessageService.java
@@ -1,19 +1,31 @@
 package com.tanaguru.service.impl;
 
+import java.util.Locale;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
  
 @Service
 public class MessageService {
 
     private final MessageSource messageSource;
+    
+    @Value("${message.lang}")
+    private String language;
 
     public MessageService(MessageSource messageSource) {
         this.messageSource = messageSource;
     }
 
     public String getMessage(String code) {
-        return messageSource.getMessage(code, null, LocaleContextHolder.getLocale());
+    	return messageSource.getMessage(code, null, getMessageLocale());
+    }
+    
+    private Locale getMessageLocale() {
+    	if(language.equals("fr")) {
+    		return Locale.FRENCH;
+    	} else {
+    		return Locale.ENGLISH;
+    	}
     }
 }
