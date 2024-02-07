98c9acaa26c08a01c2dc4ceff547312e8d415669
severine <sfoucherferet@tanaguru.com>
2024-02-07 18:40:48 +0100

    delete elementResult, statusResult & testHierarchyResult in a single query

--- tanaguru-engine/tanaguru-api/src/main/java/com/tanaguru/repository/TestHierarchyResultRepository.java
+++ tanaguru-engine/tanaguru-api/src/main/java/com/tanaguru/repository/TestHierarchyResultRepository.java
@@ -4,8 +4,14 @@ import com.tanaguru.domain.entity.audit.Audit;
 import com.tanaguru.domain.entity.audit.Page;
 import com.tanaguru.domain.entity.audit.TestHierarchy;
 import com.tanaguru.domain.entity.pageresult.TestHierarchyResult;
+import com.tanaguru.domain.entity.pageresult.TestResult;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 import java.util.Optional;
@@ -20,4 +26,9 @@ public interface TestHierarchyResultRepository extends JpaRepository<TestHierarc
     Collection<TestHierarchyResult> findAllByPage_InAndTestHierarchy(Collection<Page> pages, TestHierarchy testHierarchy);
     Collection<TestHierarchyResult> findAllByPageAndTestHierarchy(Page page, TestHierarchy testHierarchy);
     Collection<TestHierarchyResult> findAllByPage_AuditAndTestHierarchy(Audit audit, TestHierarchy testHierarchy);
+    
+    @Modifying
+    @Transactional
+    @Query("delete from TestHierarchyResult where page = :page")
+    void deleteAllInBatchByPage(@Param("page") Page page);
 }
